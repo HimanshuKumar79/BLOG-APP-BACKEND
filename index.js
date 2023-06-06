a2c0c224d4b2a3a9d44a35d6bc02b6f4d2e4d37b
Himanshu Kumar <64852520+HimanshuKumar79@users.noreply.github.com>
2023-06-06 21:00:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 require("dotenv").config();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000/",
     credentials:true
   })
 );
