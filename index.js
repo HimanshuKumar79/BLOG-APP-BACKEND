a904835e005cfc8505f3d8888bb0987676588c9c
Himanshu Kumar <64852520+HimanshuKumar79@users.noreply.github.com>
2023-06-06 21:02:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ const routes = require("./routes/route");
 const cors=require("cors")
 const PORT = process.env.PORT || 3000;
 require("dotenv").config();
+app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000/",
+    origin: "http://localhost:3000",
     credentials:true
   })
 );
 app.use(express.json());
-app.use(cookieParser());
+
 app.use("", routes);
 app.listen(PORT, () => {
   console.log("Server listening on PORT ", PORT);
