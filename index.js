66037ad3abff93956751acfa528b71912d4c9035
Himanshu Kumar <64852520+HimanshuKumar79@users.noreply.github.com>
2023-06-07 13:09:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const routes = require("./routes/route");
 const cors=require("cors")
 const PORT = process.env.PORT || 3000;
 require("dotenv").config();
-app.use(cors({origin:"blog-app-himanshu.netlify.app",credentials: true,}));
+app.use(cors({origin:"https://blog-app-himanshu.netlify.app",credentials: true,}));
 app.use(cookieParser());
 app.use(express.json());
 app.use("", routes);
