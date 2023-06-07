b132fcedefdb5ae62f246855cdf26192194ea2f6
Himanshu Kumar <64852520+HimanshuKumar79@users.noreply.github.com>
2023-06-07 12:47:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const routes = require("./routes/route");
 const cors=require("cors")
 const PORT = process.env.PORT || 3000;
 require("dotenv").config();
-app.use(cors({origin:"localhost:3000",credentials: true,}));
+app.use(cors({origin:"http://localhost:3000",credentials: true,}));
 app.use(cookieParser());
 app.use(express.json());
 app.use("", routes);
