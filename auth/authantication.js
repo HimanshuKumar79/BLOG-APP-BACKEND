cc87862604bcebb2d3928ef7050d98000e0fc87c
Himanshu Kumar <64852520+HimanshuKumar79@users.noreply.github.com>
2023-06-07 20:47:10 +0530

    Update authantication.js

--- auth/authantication.js
+++ auth/authantication.js
@@ -67,12 +67,12 @@ exports.login = async (req, res) => {
       const options = {
         maxAge: new Date(Date.now() + 9 * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: true,
-        sameSite:'none',
+//         secure: true,
+//         sameSite:'none',
       };
       const token = jwt.sign(payload, process.env.SECRET_KEY);
       res
-        .cookie("token", token,options )
+        .cookie("token", token, options)
         .status(200)
         .json({
           success: true,
