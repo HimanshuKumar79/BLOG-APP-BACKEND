5a29a0593f16c0ca1b7b9d0ad8afeceeaed175a0
Himanshu Kumar <64852520+HimanshuKumar79@users.noreply.github.com>
2023-06-07 14:07:35 +0530

    Update authantication.js

--- auth/authantication.js
+++ auth/authantication.js
@@ -67,7 +67,6 @@ exports.login = async (req, res) => {
       const options = {
         expires: new Date(Date.now() + 9 * 24 * 60 * 60 * 1000),
         secure:true,
-        sameSite:"None"
       };
       const token = jwt.sign(payload, process.env.SECRET_KEY);
       res
